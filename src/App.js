ed6d8c85630fcc243578200de269848babdb6d5a
Ryan Bigg <me@ryanbigg.com>
2019-12-16 10:43:48 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function User(props) {
+  return <div className="user">{props.name}</div>;
+}
+
+function Toot(props) {
+  return (
+    <div className="Toot">
+      <User name={props.user} />
+      {props.message}
+      {props.children}
+    </div>
+  );
+}
+
+const toots = [
+  {
+    id: 1,
+    message: "Hello Earth 616",
+    user: "Jack"
+  },
+  {
+    id: 2,
+    message: "Hello Jupiter",
+    user: "Ryan"
+  },
+  {
+    id: 3,
+    message: "Hello Saturn",
+    user: "Roddy"
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {toots.map(toot => {
+        return (
+          <Toot {...toot}>
+            <div>Extra message</div>
+          </Toot>
+        );
+      })}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const name = "Ryan Bigg";
+// const header = <h1>{name}</h1>;
+// const app = <div>{header} Example React Application</div>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
